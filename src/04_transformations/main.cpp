8b0219469d8f3fdfc89b42e1cbdb3ce186ef9645
guoyuandong <303726001@qq.com>
2024-02-23 17:55:41 +0800

    引入 glm 库

---
+++ src/04_transformations/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main() {
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans = glm::mat4(1.0f);
+	return 0;
+}
